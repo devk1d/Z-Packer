345834c410ec17bfe0b37583d7cacdd7a87d590b
wei <wei@weideMBP.lan>
2016-10-14 20:02:41 +0800

    update

--- build_scripts/libs/PackImage.js
+++ build_scripts/libs/PackImage.js
@@ -9,6 +9,7 @@ import imageminPngquant from 'imagemin-pngquant';
 import Helper from '../tools/Helper';
 import config from '../config';
 import PackJSCSS from './PackJSCSS';
+import TransformPHP from './TransformPHP';
 
 const paths = config.paths;
 const outputPath = path.join(config.paths.build, 'img');
@@ -31,20 +32,22 @@ async function packImageByDir(dirPath) {
 
     const imgFiles = glob.sync(path.join(dirPath, '*.+(jpeg|jpg|png|gif)'));
 
+    if(!imgFiles.length) return ;
+
     console.log('first: ');
     console.log(imgFiles);
 
+    let miniedFiles;
     try {
         const miniedFiles = await imagemin(imgFiles, '', {
             plugins: [
-                imageminMozjpeg(),
+                imageminMozjpeg({quality: '65-80'}),
                 imageminPngquant({quality: '65-80'})
             ]
         });
     }catch(e) {
         console.log(e);
     }
-
 
     console.log(miniedFiles);
     console.log('second: ');
@@ -83,6 +86,51 @@ function removeOldFile(outputName) {
 function replaceImgPath(filePathArr, imgNameReplaceObj) {
     console.log(filePathArr);
     console.log(imgNameReplaceObj);
+
+    filePathArr.forEach(filePath => {
+        const fileExt = path.parse(filePath).ext;
+        const fileContent = fs.readFileSync(filePath, { encoding: 'utf8'});
+
+        for (let imgName in imgNameReplaceObj) {
+            const replaceName = imgNameReplaceObj[imgName];
+
+            if(fileExt == 'js') {
+                //ex: src='test.png'
+                _reg =  new RegExp('\''+ imgName +'\'', 'g');
+                fileContent = fileContent.replace(_reg, 'window.STATIC_HOST+\'page/img/' + replaceName + '\'');
+                //ex: src="test.png"
+                _reg =  new RegExp('"'+ imgName +'"', 'g');
+                fileContent = fileContent.replace(_reg, 'window.STATIC_HOST+"page/img/' + replaceName + '"');
+                //ex: "url(test.png)"
+                _reg =  new RegExp('"url\\('+ imgName +'\\)"', 'g');
+                fileContent = fileContent.replace(_reg, '"url("+window.STATIC_HOST+"page/img/' + replaceName + ')"');
+                //ex: 'url(test.png)'
+                _reg =  new RegExp('\'url\\('+ imgName +'\\)\'', 'g');
+                fileContent = fileContent.replace(_reg, '\'url(\'+window.STATIC_HOST+\'page/img/' + replaceName + ')\'');
+            } else if(fileExt == 'php') {
+                //ex: "background: url(test.png)";
+                _reg =  new RegExp('url\\('+ imgName +'\\)', 'g');
+                fileContent = fileContent.replace(_reg, 'url({=STATIC_HOST}page/img/' + replaceName + ')');
+                //ex: img src='test.png'
+                _reg =  new RegExp('\''+ imgName +'\'', 'g');
+                fileContent = fileContent.replace(_reg, '\'{=STATIC_HOST}page/img/' + replaceName + '\'');
+                //ex: img src="test.png"
+                _reg =  new RegExp('"'+ imgName +'"', 'g');
+                fileContent = fileContent.replace(_reg, '"{=STATIC_HOST}page/img/' + replaceName + '"');
+            } else if(fileExt == 'css' || fileExt == 'less') {
+                //ex: background: url(test.png)
+                _reg =  new RegExp('url\\('+ imgName +'\\)', 'g');
+                fileContent = fileContent.replace(_reg, 'url(/page/img/' + replaceName + ')');
+            }
+
+            if (fileExt == 'php') {
+                fileContent = TransformPHP(fileContent);
+            }
+        }
+
+
+    })
+
 }
 
 /*
